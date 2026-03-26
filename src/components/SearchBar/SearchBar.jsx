0090cb380f68fd2622ebc741ef02e89ab8a2aa32
Zuxriddin Hasanov <zuhriddin.h.011@email.com>
2026-03-26 18:00:07 +0500

    feat: creat secrh bar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,19 @@
+import { SearchIcon } from '../Icons/Icons';
+import styles from './SearchBar.module.scss';
+
+export default function SearchBar({ placeholder, value, onChange }) {
+  return (
+    <div className={styles.searchBar}>
+      <span className={styles.icon}>
+        <SearchIcon />
+      </span>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className={styles.input}
+      />
+    </div>
+  );
+}
